8c4184f403fe6c730f30fe9db26e0ac9e18f6a42
Michal Gebauer <mishak@mishak.net>
2013-08-13 18:15:05 +0200

    Trait CompilerExtensionSugar bringing you the sweetness

---
+++ src/Rixxi/Modular/DI/CompilerExtensionSugar.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Rixxi\Modular\DI;
+
+use Nette;
+use Nette\Utils\Validators;
+
+
+/**
+ * Trait only purpose is to remove unused
+ */
+trait CompilerExtensionSugar
+{
+
+	/**
+	 * @param string $interfaceOrClass
+	 * @return Nette\DI\CompilerExtension
+	 * @throws Nette\Utils\AssertionException
+	 */
+	private function getCompilerExtension($interfaceOrClass)
+	{
+		foreach ($this->compiler->getExtensions() as $extension) {
+			if ($extension instanceof $interfaceOrClass) {
+				return $extension;
+			}
+		}
+
+		throw new Nette\Utils\AssertionException("Please register the required $interfaceOrClass to Compiler.");
+	}
+
+
+	/**
+	 * @param string $interfaceOrClass
+	 * @return Nette\DI\CompilerExtension[]
+	 */
+	private function getCompilerExtensions($interfaceOrClass)
+	{
+		$extensions = [];
+		foreach ($this->compiler->getExtensions() as $extension) {
+			if ($extension instanceof $interfaceOrClass) {
+				$extensions[] = $extension;
+			}
+		}
+		return $extensions;
+	}
+
+
+	/**
+	 * Load __DIR__/config/$name.neon services with prefix $name
+	 * @param string $name
+	 * @author Filip Procházka (filip@prochazka.su)
+	 * @see copy of package Kdyby/Doctrine class Kdyby\Doctrine\DI\OrmExtension function loadConfig code :)
+	 */
+	private function loadConfig($name)
+	{
+		$this->compiler->parseServices(
+			$this->getContainerBuilder(),
+			$this->loadFromFile(__DIR__ . '/config/' . $name . '.neon'),
+			$this->prefix($name)
+		);
+	}
+
+}
